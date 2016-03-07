280a2fe364220370ef437c7d4e0956fb429c0989
mac <mac@macdeMacBook-Air.local>
2016-03-07 20:39:12 +0800

    Initial Commit

---
+++ 图片自适应/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片自适应
+//
+//  Created by mac on 16/3/7.
+//  Copyright (c) 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
